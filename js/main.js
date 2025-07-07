98b28a1f8df6ca24951671b779521ba56082db4d
m2web <m2web@users.noreply.github.com>
2025-07-07 17:29:37 +0000

    deploy: 88e5b5535d14f335dc2004c1d593f0762c679f77

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
 //set as string
     if (hours   < 10) {hours   = ""+hours;}
-    if (minutes < 10) {minutes = ""+minutes;}
+    if (minutes < 10) {minutes = "0"+minutes;}
     if (seconds < 10) {seconds = "0"+seconds;}
 
     if(hours > 0){
